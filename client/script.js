301f28bc5951e5948513183dc3bcf141c9a067c9
wmitchel <williamjamesmitchel@gmail.com>
2015-11-17 20:31:47 -0500

    Add mad lib replace top text. Add top text fade in.

--- client/script.js
+++ client/script.js
@@ -1,11 +1,18 @@
 Meteor.startup(function () {
 	$("#generate").click(function (event) {
 		event.preventDefault();
+		$("#mad-lib-text").css("opacity", "0");
 		let exclamation = $("#inputExclamation").val();
 		let adverb = $("#inputAdverb").val();
 		let noun = $("#inputNoun").val();
 		let verb = $("#inputVerb").val();
 		let madLib = "'"+exclamation+"' he said, as he " + adverb + " jumped onto his " + noun + " and " + verb + " into the sunset.";
 		console.log(madLib);
+
+		$("#mad-lib-text").html(madLib);
+		$("#mad-lib-text").css("color", "#0000ff");
+		$("#mad-lib-text").animate({
+			opacity: 1
+		}, 2000, function(){});
 	});
 });
