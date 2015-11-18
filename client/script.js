d5a02168e91b5e73323e1430feea6bbea47b53aa
naikamish <naikamish0822@gmail.com>
2015-11-17 20:14:47 -0500

    Add function to generate mad lib when generate button pressed and log to console

--- client/script.js
+++ client/script.js
@@ -1,6 +1,11 @@
 Meteor.startup(function () {
 	$("#generate").click(function (event) {
 		event.preventDefault();
-		console.log("Hello, world!");
+		let exclamation = $("#inputExclamation").val();
+		let adverb = $("#inputAdverb").val();
+		let noun = $("#inputNoun").val();
+		let verb = $("#inputVerb").val();
+		let madLib = "'"+exclamation+"' he said, as he " + adverb + " jumped onto his " + noun + " and " + verb + " into the sunset.";
+		console.log(madLib);
 	});
 });
